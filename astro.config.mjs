d6431d4426aafa24b09c2b81bf3d74fdf00362b7
ccg1024 <aimerchenyuan@163.com>
2023-02-01 14:48:58 +0800

    add react and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+import react from "@astrojs/react";
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react(), image()]
 });
