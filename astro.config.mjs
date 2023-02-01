62a15dbdf75965bc1e1d21543b3affa3b67b0f98
ccg1024 <aimerchenyuan@163.com>
2023-02-01 14:52:30 +0800

    add tailwind and goole font

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 // https://astro.build/config
 import image from "@astrojs/image";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), react(), image()]
+  integrations: [sitemap(), react(), image(), tailwind()]
 });
